155ccb3df249d9323bceb6433b8f622b20b05e06
Guanisnt <allen6010101@gmail.com>
2024-07-07 20:06:38 +0800

    Longest common subsequence

--- Dynamic Programming/Longest Cimmin Sucsequence DP.cpp
+++ Dynamic Programming/Longest Cimmin Sucsequence DP.cpp
@@ -4,6 +4,20 @@
 #include <string>
 using namespace std;
 
+/**
+ * 1. subproblem
+ *  - LCS of str1[0:i] and str2[0:j]
+ * 2. guess
+ *  - Last character of str1 and str2 are the same or not
+ * 3. recurrence
+ *  - dp[i][j] = dp[i-1][j-1] + 1 if str1[i] == str2[j]
+ *  - dp[i][j] = max(dp[i-1][j], dp[i][j-1]) if str1[i] != str2[j]
+ * 4. topological order
+ *  - increasing substring size
+ * 5. original problem
+ *  - dp(str1.size(), str2.size())
+ */
+
 int LCS(string str1, string str2) {
     vector<vector<int>> dp(str1.size()+1, vector<int>(str2.size()+1, 0));
     for(int i=1; i<str1.size()+1; i++) {
