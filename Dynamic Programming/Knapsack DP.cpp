5c8b5a43954f6118f1197555e302c2f76b41fab5
Guanisnt <allen6010101@gmail.com>
2024-07-08 19:13:07 +0800

    knapsack

--- Dynamic Programming/Knapsack DP.cpp
+++ Dynamic Programming/Knapsack DP.cpp
@@ -3,42 +3,36 @@
 #include <algorithm>
 using namespace std;
 
-// Returns the value of maximum profit
-int knapSackRec(int W, vector<int>& wt, vector<int>& val, int index, vector<vector<int>>& dp) {
-    // base condition
-    if (index < 0)
-        return 0;
-    if (dp[index][W] != -1)
-        return dp[index][W];
+/**
+ * 1. guess
+ * -  is the item i in the knapsack or not -> 2 choices
+ * 2. subproblem
+ * -  suffix i: of items & remaining capacity w(cannot exceed W)
+ * 3. recurrence
+ * -  dp[i][w] = max(dp[i+1][w], dp[i+1][w-weight[i]] + profit[i)
+ * 4. topological order
+ * -  decreasing suffix i
+ * 5. original problem
+ * -  dp[0][W]
+ */
 
-    if (wt[index] > W) {
-
-        // Store the value of function call
-        // stack in table before return
-        dp[index][W] = knapSackRec(W, wt, val, index - 1, dp);
-        return dp[index][W];
-    } else {
-        // Store value in a table before return
-        dp[index][W] = max(val[index]
-                           + knapSackRec(W - wt[index], wt, val,
-                                         index - 1, dp),
-                       knapSackRec(W, wt, val, index - 1, dp));
-
-        // Return value of table after storing
-        return dp[index][W];
+void knapsack(vector<int>& profit, vector<int>& weight, int W) {
+    vector<vector<int>> dp(profit.size()+1, vector<int>(W+1, 0));
+    for(int i=profit.size()-1; i>=0; i--) {
+        for(int w=0; w<=W; w++) {
+            if(w >= weight[i]) {
+                dp[i][w] = max(dp[i+1][w], dp[i+1][w-weight[i]] + profit[i]);
+            } else {
+                dp[i][w] = dp[i+1][w];
+            }
+        }
     }
-}
-
-int knapSack(int W, vector<int>& wt, vector<int>& val, int n) {
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
-    return knapSackRec(W, wt, val, n - 1, dp);
+    cout << dp[0][W] << endl;
 }
 
 int main() {
     vector<int> profit = { 60, 100, 120 };
     vector<int> weight = { 10, 20, 30 };
-    int W = 50;
-    int n = sizeof(profit) / sizeof(profit[0]);
-    cout << knapSack(W, weight, profit, n);
+    knapsack(profit, weight, 50);
     return 0;
 }
