9f8f70bfb8c166c10023c05184d1cffa50e4ee2d
Guanisnt <allen6010101@gmail.com>
2024-09-18 16:10:44 +0800

    Prim's Algorithm

--- MST/Prim's algorithm.cpp
+++ MST/Prim's algorithm.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <tuple>
 #include <queue>
 using namespace std;
 #define V 5
 
-void primMST(int graph[V][V]) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    vector<int> key(V, INT_MAX);
-    vector<int> parent(V, -1);
-    vector<bool> inMST(V, false);
-    
+vector<tuple<int, int, long long>> primMST(vector<vector<int>>& graph) {
+    priority_queue<tuple<long long, int, int>, vector<tuple<long long, int, int>>, greater<tuple<long long, int, int>>> pq;
+    pq.push({0, 0, -1}); // {weight, starting vertex, parent}
+    vector<bool> visited(V, false);
+    vector<tuple<int, int, long long>> mst; // to store (u, v, weight) of MST
+    while(!pq.empty()) {
+        auto [w, u, parent] = pq.top();  // u 是當前節點, parent 是 u 的 parent
+        pq.pop();
+        if(visited[u]) continue;
+        visited[u] = true;
+        if(parent != -1) mst.push_back({parent, u, w});
+        for(int v=0; v<V; v++) {  // 遍歷目前節點 u 的所有鄰居
+            if(!visited[v] && graph[u][v] != 0) {
+                pq.push({graph[u][v], v, u});  // 把 {weight, neighbor vertex, current vertex(parent)} 放進 priority queue
+            }
+        }
+    }
+    return mst;
 }
 
 int main() {
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
+    vector<vector<int>> graph = 
+                        { { 0, 2, 0, 6, 0 },
+                          { 2, 0, 3, 8, 5 },
+                          { 0, 3, 0, 0, 7 },
+                          { 6, 8, 0, 0, 9 },
+                          { 0, 5, 7, 9, 0 } };
 
-    primMST(graph);
+    vector<tuple<int, int, long long>> mst = primMST(graph);  // (u, v, weight)
+    
+    cout << "Edges in the MST:" << endl;
+    for (auto &[u, v, w] : mst) {
+        cout << "Edge: " << u << " - " << v << " | Weight: " << w << endl;
+    }
+
+    int totalWeight = 0;
+    for (auto &[u, v, w] : mst) {
+        totalWeight += w;
+    }
+    cout << "Total weight of MST: " << totalWeight << endl;
 
     return 0;
 }
